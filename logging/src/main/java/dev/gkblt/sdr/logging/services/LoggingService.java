a6a498d58419a470b706290255c7084338d87388
Sedar Gokbulut <sedar.gokbulut@gmail.com>
2023-05-23 16:03:44 -0700

    Defining log buffer capacity, so the buffer doesn't grow indefinitely

--- logging/src/main/java/dev/gkblt/sdr/logging/services/LoggingService.java
+++ logging/src/main/java/dev/gkblt/sdr/logging/services/LoggingService.java
@@ -1,28 +1,35 @@
 package dev.gkblt.sdr.logging.services;
 
 import dev.gkblt.sdr.logging.data.LogEntry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.PriorityQueue;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 @Service
 public class LoggingService implements ILogging {
 
-    PriorityQueue<LogEntry> entries = new PriorityQueue<>((a, b) -> (int)(a.timestamp() - b.timestamp()));
+    Logger logger = LoggerFactory.getLogger(LoggingService.class);
+    @Value("${log.buffer.capacity}")
+    private int logBufferCapacity;
+    TreeMap<Long, LogEntry> entries = new TreeMap<>();
 
     @Override
     public void log(LogEntry entry) {
-       entries.offer(entry);
+       entries.put(entry.timestamp(),entry);
+       while (entries.size() > logBufferCapacity) {
+           entries.pollFirstEntry();
+       }
     }
 
     @Override
     public List<LogEntry> get(Optional<Long> afterTimestamp) {
-        return entries.stream()
-                .dropWhile((e) -> e.timestamp() <= afterTimestamp.orElse(0L))
-                .collect(Collectors.toList());
+        return List.copyOf(entries.tailMap(afterTimestamp.orElse(0L)+1).values());
     }
 }
--- logging/src/test/java/dev/gkblt/sdr/logging/services/LoggingServiceTest.java
+++ logging/src/test/java/dev/gkblt/sdr/logging/services/LoggingServiceTest.java
@@ -1,41 +1,47 @@
 package dev.gkblt.sdr.logging.services;
 
 import dev.gkblt.sdr.logging.data.LogEntry;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@SpringBootTest
+@TestPropertySource(
+        locations = "classpath:unittest.properties"
+)
 class LoggingServiceTest {
 
-    @BeforeEach
-    void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
-    }
+    @Autowired
+    private ILogging service;
 
     @Test
     void log() {
+        long time = 0;
+        while(time < 1000) {
+            service.log(new LogEntry(++time, String.format("%d", time)));
+        }
+        assertEquals(100, service.get(Optional.empty()).size());
     }
 
     @Test
     void get() {
         long time = 0;
-        LoggingService service = new LoggingService();
         assertEquals(List.of(),service.get(Optional.empty()));
         service.log(new LogEntry(++time, String.format("%d",time)));
         assertEquals(List.of(new LogEntry(time, String.format("%d",time))),service.get(Optional.empty()));
         while(time < 1000) {
             service.log(new LogEntry(++time, String.format("%d", time)));
         }
-        assertEquals(1000, service.get(Optional.empty()).size());
-        assertEquals(300, service.get(Optional.of(700L)).size());
+        var all = service.get(Optional.empty());
+        assertEquals(100, all.size());
+        var last30 = service.get(Optional.of(970L));
+        assertEquals(30, last30.size());
         assertEquals(time, service.get(Optional.of(time-1)).get(0).timestamp());
     }
 
